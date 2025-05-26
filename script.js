b8f5295702b53f0628fd02519cc201e85f92ea0d
Anjali <sj5065070@gmail.com>
2025-05-26 07:38:17 +0530

    This is my new files

---
+++ script.js
@@ -0,0 +1,16 @@
+var body=document.querySelector('body')
+document.querySelector('.red').addEventListener('click',()=>{
+  body.style.backgroundColor="rgb(222, 131, 131)"
+})
+document.querySelector('.blue').addEventListener('click',()=>{
+  body.style.backgroundColor="rgb(75, 48, 232)"
+})
+document.querySelector('.yellow').addEventListener('click',()=>{
+  body.style.backgroundColor="rgb(224, 236, 113)"
+})
+document.querySelector('.green').addEventListener('click',()=>{
+  body.style.backgroundColor="rgb(91, 236, 43)"
+})
+document.querySelector('.orange').addEventListener('click',()=>{
+  body.style.backgroundColor="rgb(250, 142, 0)"
+})
